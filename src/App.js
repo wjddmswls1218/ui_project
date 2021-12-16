397e49aff3f5d5699dcf1b1dbd2d48dca661f94e
wjddmswls1218 <ouhouh2222@gmail.com>
2021-12-16 20:53:58 +0900

    antd Switch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    scv: false,
+  };
+
+  _checkHandler = (checked) => {
+    console.log(checked);
+
+    this.setState((prev) => {
+      return {
+        scv: checked,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <section>
+        <h3>Switch {this.state.scv ? "체크 되었어요" : "체크 안되었어요"}</h3>
+        <Switch
+          defaultChecked={false}
+          checked={this.state.scv}
+          onChange={this._checkHandler}
+        />
+      </section>
+    );
+  }
 }
 
 export default App;
